930dd19d905cecd986e0f7255eea5584bdf45a61
Kayla Rampersaud <krampersaud98@gmail.com>
2026-01-22 01:03:32 -0500

    add to index.js

--- index.js
+++ index.js
@@ -11,7 +11,13 @@
  * echo("test", 1); // "test"
  */
 export function echo(word, n) {
-  // TODO
+  let result = "";
+
+  for (let i = 0; i < n; i++) {
+    result = result + word;
+  }
+
+  return result;
 }
 
 /**
@@ -27,7 +33,22 @@ export function echo(word, n) {
  * echoWithSpace("test", 1); // "test"
  */
 export function echoWithSpace(word, n) {
-  // TODO
+  if (n <= 0 || word.length === 0) return "";
+
+  let result = "";
+  let i = 0;
+
+  while (i < n) {
+    result += word;
+
+    if (i < n - 1) {
+      result += " ";
+    }
+
+    i++;
+  }
+
+  return result;
 }
 
 /**
@@ -42,7 +63,15 @@ export function echoWithSpace(word, n) {
  * sumTo(100); // 5050
  */
 export function sumTo(n) {
-  // TODO
+  let result = 0;
+  let i = 1;
+
+  while (i <= n) {
+    result += i;
+    i++;
+  }
+
+  return result;
 }
 
 /**
@@ -58,7 +87,15 @@ export function sumTo(n) {
  * sumFromTo(2, 2); // 2
  */
 export function sumFromTo(a, z) {
-  // TODO
+  let result = 0;
+  let i = a;
+
+  while (i <= z) {
+    result += i;
+    i++;
+  }
+
+  return result;
 }
 
 /**
@@ -73,7 +110,12 @@ export function sumFromTo(a, z) {
  * countdown(5); // logs 5, 4, 3, 2, 1
  */
 export function countdown(n) {
-  // TODO
+  let i = n;
+
+  while (i > 0) {
+    console.log(i);
+    i--;
+  }
 }
 
 /**
@@ -88,7 +130,17 @@ export function countdown(n) {
  * sumOddsToN(1); // 1
  */
 export function sumOddsToN(n) {
-  // TODO
+  let result = 0;
+  let i = 1;
+
+  while (i <= n) {
+    if (i % 2 !== 0) {
+      result += i;
+    }
+    i++;
+  }
+
+  return result;
 }
 
 /**
@@ -108,7 +160,14 @@ export function sumOddsToN(n) {
  * getGrowthTime(5, 78); // 80
  */
 export function getGrowthTime(start, target) {
-  // TODO
+  if (start <= 0) return undefined;
+
+  let i = 0;
+  while (start < target) {
+    start *= 2;
+    i += 1;
+  }
+  return i * 20;
 }
 
 /**
@@ -128,7 +187,14 @@ export function getGrowthTime(start, target) {
  * getCompoundTime(30000, 0.04, 50000); // 14
  */
 export function getCompoundTime(start, rate, target) {
-  // TODO
+  if (start <= 0 || rate <= 0) return undefined;
+
+  let i = 0;
+  while (start < target) {
+    start *= 1 + rate;
+    i += 1;
+  }
+  return i;
 }
 
 /**
@@ -154,7 +220,21 @@ export function getCompoundTime(start, rate, target) {
  * moveWater(7, 3); // 1
  */
 export function moveWater(colander, bucket) {
-  // TODO
+  if (colander <= 0) return undefined;
+  if (bucket <= 0) return 0;
+  if (colander >= bucket) return 1;
+  if (colander === 1) return bucket;
+
+  let totalWater = 0;
+  let trips = 0;
+
+  while (totalWater < bucket) {
+    totalWater += colander;
+    colander = Math.max(1, colander - 1);
+    trips++;
+  }
+
+  return trips;
 }
 
 /**
@@ -174,5 +254,18 @@ export function moveWater(colander, bucket) {
  * fizzbuzz(15); // logs 1, 2, fizz, 4, buzz, fizz, 7, 8, fizz, buzz, 11, fizz, 13, 14, fizzbuzz
  */
 export function fizzbuzz(n) {
-  // TODO
+  let i = 1;
+
+  while (i <= n) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("fizzbuzz");
+    } else if (i % 3 === 0) {
+      console.log("fizz");
+    } else if (i % 5 === 0) {
+      console.log("buzz");
+    } else {
+      console.log(i);
+    }
+    i++;
+  }
 }
